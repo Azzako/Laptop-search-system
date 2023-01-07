2c50f65ee7d0b56906b7228518f4b7f0b774bcd7
New-Bucket <azizjonnormatov437@gmail.com>
2023-01-07 10:18:16 +0500

    file

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,7 +9,7 @@ class SearchesController < ApplicationController
         @search = Search.create(search_params)
         redirect_to @search 
     end
-    
+     
     def show 
         @search = Search.find(params[:id])
     end
